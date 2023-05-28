ed6da70034c1ba1638c2864402e2dbe746197854
Shawn McCollum <mshawn233@gmail.com>
2023-05-28 16:33:20 -0400

    Add functions to handle hello chat requests and chat messages

--- server/server.go
+++ server/server.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"log"
 
-	"mshawn233/cs544-project-part-3/myproto"
+	"mshawn233/cs544-project-part-3/chatmessagetypes"
+	"mshawn233/cs544-project-part-3/tls"
 
 	"github.com/quic-go/quic-go"
 )
 
-var recvBuffer = make([]byte, myproto.MAX_PACKET_SZ)
+const MAX_PACKET_SZ = 1 << 16
+
+var recvBuffer = make([]byte, MAX_PACKET_SZ)
 
 func InitServer(addr string) (quic.Connection, error) {
 	log.Printf("Server is initializing")
-	listener, err := quic.ListenAddr(addr, myproto.GenerateTLSConfig(), nil)
+	listener, err := quic.ListenAddr(addr, tls.GenerateTLSConfig(), nil)
 	if err != nil {
 		return nil, err
 	}
@@ -24,15 +26,9 @@ func InitServer(addr string) (quic.Connection, error) {
 	return listener.Accept(context.Background())
 }
 
-func HandleStream(conn quic.Connection) error {
-	log.Printf("Server is waiting for a connection")
-	stream, err := conn.AcceptStream(context.Background())
-	if err != nil {
-		return err
-	}
-	log.Printf("Just Got A Connection")
+func HandleHelloChatRequest(stream quic.Stream) error {
 
-	//n, err := io.ReadFull(stream, recvBuffer)
+	//Read the request from the client
 	n, err := stream.Read(recvBuffer)
 	log.Printf("Post Readfull")
 	if (err != nil) && (err != io.ErrUnexpectedEOF) {
@@ -41,41 +37,75 @@ func HandleStream(conn quic.Connection) error {
 	}
 	log.Printf("SVR: Received %d bytes", n)
 
-	pckt, _ := myproto.FromBytes(recvBuffer[0:n])
-	log.Printf("RECEIVED\n----------\n%s\n----------", pckt.ToString())
+	pckt, _ := chatmessagetypes.HelloChatRequestFromBytes(recvBuffer[0:n])
+	log.Printf("Received request from user %s\n", pckt.Username)
 
-	//Now Lets Send A Response, First Lets Build An Echo Packet
-	pckt.ProtoFlag = myproto.FLG_SND_ACK
-	pckt.Payload = []byte(fmt.Sprintf("ECHO->%0.25s", pckt.Payload))
-	pckt.Length = len(pckt.Payload)
+	//Generate HelloChatResponse
+	hcr := chatmessagetypes.NewHelloChatResponse(0x01)
 
 	//Now lets convert into bytes
-	netBytes, err := myproto.ToBytes(pckt)
+	netBytes, err := chatmessagetypes.HelloChatResponseToBytes(hcr)
 	if err != nil {
 		log.Printf("Error serializing: %+v", err)
 		return err
 	}
 
-	//Send to the server
+	//Send response to the client
 	n, err = stream.Write(netBytes)
 	if err != nil {
 		log.Printf("Error writing to client: %+v", err)
 		return err
 	}
-	log.Printf("Just wrote %d bytes to client\n%s\n", n, pckt.ToString())
-
-	//This is a stream so we need to have a way for the client to have the opportunity to receive
-	//the message and then close the connection, we use context for this
-	connCtx := conn.Context()
-	<-connCtx.Done()
+	log.Printf("Just wrote %d bytes to user %s\n", n, pckt.Username)
 
 	return nil
 }
 
+func HandleChatMessage(stream quic.Stream) error {
+
+	//Read the request from the client
+	n, err := stream.Read(recvBuffer)
+	log.Printf("Post Readfull")
+	if (err != nil) && (err != io.ErrUnexpectedEOF) {
+		log.Printf("GOT ERROR READING FROM CLIENT %+v", err)
+		return err
+	}
+	log.Printf("SVR: Received %d bytes", n)
+
+	pckt, _ := chatmessagetypes.ChatMessageFromBytes(recvBuffer[0:n])
+	log.Printf("Received chat message %s\n", pckt.ChatMessageData)
+
+	//Send response to the client
+	n, err = stream.Write(recvBuffer[0:n])
+	if err != nil {
+		log.Printf("Error writing to client: %+v", err)
+		return err
+	}
+	log.Printf("Just wrote %d bytes to client\n", n)
+
+	return nil
+
+}
+
 func main() {
+
 	c, err := InitServer("localhost:4242")
 	log.Printf("Server just initialized, error is %+v", err)
-	HandleStream(c)
 
-	//time.Sleep(time.Second * 2)
+	log.Printf("Server is waiting for a connection")
+	stream, err := c.AcceptStream(context.Background())
+	if err != nil {
+		log.Printf("Error accepting stream: %+v", err)
+	}
+	log.Printf("Just Got A Connection")
+
+	HandleHelloChatRequest(stream)
+
+	HandleChatMessage(stream)
+
+	//This is a stream so we need to have a way for the client to have the opportunity to receive
+	//the message and then close the connection, we use context for this
+	connCtx := c.Context()
+	<-connCtx.Done()
+
 }
